4a41c91b2ea89e259808827c49810115ebae18d2
danila <dm.perlov@jet.su>
2020-08-01 16:03:00 +0300

    update

--- app/tabs/experience/jet.jsx
+++ app/tabs/experience/jet.jsx
@@ -13,8 +13,7 @@ export const JetExperience = () => (
       </Card.Header>
       <Accordion.Collapse eventKey="0">
         <Card.Body>
-          VTB DBO project concerned of distant banking operations. Stream
-          bank-support-contract. Support of bank-client contract communication
+          VTB DBO project concerned of distant banking operations. Support of bank-client contract communication
           in terms of the Russian Federal Law 214.
         </Card.Body>
       </Accordion.Collapse>
@@ -27,16 +26,17 @@ export const JetExperience = () => (
       </Card.Header>
       <Accordion.Collapse eventKey="1">
         <Card.Body>
+          <p>Acting as a team lead, putting out fire when needed</p>
+          <p>Implementing new modules</p>
           <p>Fixing bugs</p>
+          <p>Refactoring tons of codebase</p>
+          <p>Making Code-review</p>
+          <p>Keeping codebase tuned for eternally changing circumstances</p>
           <p>Fixing technical setting</p>
           <p>Comunicating with buisnes analysts</p>
           <p>Comunicating with frontend and backend teams</p>
-          <p>Keeping codebase tuned for internally changing circumstances</p>
-          <p>Implementing new modules</p>
-          <p>Refactoring tons of codebase</p>
-          <p>Making Code-review</p>
           <p>Consult and help team members</p>
-          <p>Update external streams and platform interations</p>
+          <p>Update external streams and platform iterations</p>
         </Card.Body>
       </Accordion.Collapse>
     </Card>
